531f44a7404fca5e2a858a70ca1e9f162721a6bf
EstefanHu <estefanhu074@gmail.com>
2019-08-19 03:11:36 -0700

    Installed Update location modal

--- uwBreatheAdmin/js/node.js
+++ uwBreatheAdmin/js/node.js
@@ -1,6 +1,10 @@
 var modal = document.getElementById("createLocationModal");
 var btn = document.getElementById("createLocationButton");
 var span = document.getElementsByClassName("close")[0];
+var modalUpdate = document.getElementById("updateLocationModal");
+var btnUpdate = document.getElementById("updateLocationButton");
+var spanUpdate = document.getElementsByClassName("close")[1];
+
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -11,5 +15,14 @@ span.onclick = function() {
 window.onclick = function(e) {
     if (e.target == modal) {
         modal.style.display = "none";
+    } 
+    if (e.target == modalUpdate) {
+        modalUpdate.style.display = "none";
     }
+}
+btnUpdate.onclick = function () {
+    modalUpdate.style.display = "block";
+}
+spanUpdate.onclick = function () {
+    modalUpdate.style.display = "none";
 }
