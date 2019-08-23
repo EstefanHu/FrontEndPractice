ba24267077c2947d7a328e8af92da24c27f354f1
EstefanHu <estefanhu074@gmail.com>
2019-08-22 19:00:43 -0700

    Finished intro

--- p5/app.js
+++
@@ -1,8 +0,0 @@
-let canvasElement = document.getElementById("my-canvas");
-let canvasContext = canvasElement.getContext("2d");
-
-canvasContext.lineWidth = 10;
-canvasContext.strokeStyle = 'blue';
-canvasContext.strokeRect(30, 10, 70, 70);
-canvasContext.fillStyle = 'red';
-canvasContext.fillRect(50, 30, 40, 40);
