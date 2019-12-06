4bb7f6158cd1753f48d59b1b11ec52f4ca891bcc
Estefan Hu <estefanhu074@gmail.com>
2019-12-06 11:39:29 -0800

    initialized public directory

--- Choropleth/app.js
+++ Choropleth/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
