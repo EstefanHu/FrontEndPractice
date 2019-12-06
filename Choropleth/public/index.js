82149dad0be5f1cd75881751a0a187a18c53b489
Estefan Hu <estefanhu074@gmail.com>
2019-12-06 12:07:26 -0800

    installed color getter

--- Choropleth/public/index.js
+++ Choropleth/public/index.js
@@ -3,6 +3,29 @@
   window.addEventListener('load', init);
 
   function init() {
-    console.log('hello world');
+    startMap();
+  }
+
+  function startMap() {
+    let mapboxAccessToken = 'pk.eyJ1IjoiZXN0ZWZhbjA3NCIsImEiOiJjanozbm83OXUwNHRqM25tcm84MHYxd3cyIn0.MSuA8qUuoo1A2aWOcXI5ng';
+    let map = L.map('map').setView([37.8, -96], 4);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+      id: 'mapbox/light-v9'
+    }).addTo(map);
+
+    L.geoJson(statesData).addTo(map);
+
+  }
+
+  function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
   }
 })();
