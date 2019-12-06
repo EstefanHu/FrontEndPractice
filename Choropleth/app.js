925fca94317c334783da5344c12cc1e79afccf45
Estefan Hu <estefanhu074@gmail.com>
2019-12-06 11:43:57 -0800

    set up file server

--- Choropleth/app.js
+++ Choropleth/app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.static('public'));
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
